e7167881b44fa0309823cdac752fbef5ed4dafec
samuel.madrazo <samuel.madrazo@etendo.software>
2026-09-19 18:58:17 -0300

    Segundo commit de la demo peluqueria/belleza2

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Team from "./components/Team";
 import Testimonials from "./components/Testimonials";
 import CTA from "./components/CTA";
 import Footer from "./components/Footer";
+import WhatsAppFloat from "./components/WhatsAppFloat";
 
 function App() {
   return (
@@ -24,6 +25,8 @@ function App() {
       </main>
 
       <Footer />
+
+      <WhatsAppFloat />
     </>
   );
 }
